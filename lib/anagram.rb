481608b53475830a9ae1f0b2a37c4f68fd551a0b
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-08 14:21:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :word, :sentence
+
+  def initialize(word)
+    @word = word
+  end
+
+  # first version
+  # def match(sentence)
+  #   anagram = []
+  #   sentence.each do |word|
+  #     if word.downcase.split("").sort == @word.downcase.split("").sort
+  #       anagram << word
+  #     end
+  #   end
+  #   anagram
+  # end
+
+
+  def match(sentence)
+    sentence.select do |word|
+      word.downcase.split("").sort == @word.downcase.split("").sort
+      end
+  end
+
+end
