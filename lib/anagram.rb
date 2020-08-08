aa9acdd603b45486a4118f9a9078fa138873d0f5
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-08 13:53:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     sentence.detect do |word|
       # binding.pry
       if word.downcase.split("").sort == @word.downcase.split("").sort
-
+        binding.pry
         return %w(word)
       else
         return []
